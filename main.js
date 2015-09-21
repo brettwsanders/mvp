f466115b1816f76b5d727fd2aa05ab01b1ea4a0a
Brett Sanders <bsand08@gmail.com>
2015-09-21 16:30:40 -0700

    Convert current frame information from an array to an object of arrays.

--- main.js
+++ main.js
@@ -1,57 +1,96 @@
 var canvas = document.getElementById('frameView');
 var context = canvas.getContext("2d");
 
-var clickX = [];
-var clickY = [];
-var clickDrag = [];
+//information for current frame
+var currentFrame = {
+  clickX: [],
+  clickY: [],
+  clickDrag: []
+};
+
+// var clickX = [];
+// var clickY = [];
+// var clickDrag = [];
+
+//information for prior frame
+// var previousFrame = {};
+// var prevClickX = [];
+// var prevClickY = [];
+// var prevClickDrag = [];
+
 var paint;
 var framesSaved = 0;
-var currentFrame = 1;
+var currentFrameID = 1;
 var playInterval;
 
 var addClick = function (x, y, dragging) {
-  clickX.push(x);
-  clickY.push(y);
-  clickDrag.push(dragging);
+  currentFrame.clickX.push(x);
+  currentFrame.clickY.push(y);
+  currentFrame.clickDrag.push(dragging);
 };
 
-var redraw = function () {
+var redraw = function (frame) {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
   context.lineJoin = "round";
   context.lineWidth = 7;
       
-  for(var i=0; i < clickX.length; i++) {    
+  for(var i=0; i < frame.clickX.length; i++) {    
     context.beginPath();
-    if(clickDrag[i] && i){
-      context.moveTo(clickX[i-1], clickY[i-1]);
+    if(frame.clickDrag[i] && i){
+      context.moveTo(frame.clickX[i-1], frame.clickY[i-1]);
      }else{
-       context.moveTo(clickX[i]-1, clickY[i]);
+       context.moveTo(frame.clickX[i]-1, frame.clickY[i]);
      }
-     context.lineTo(clickX[i], clickY[i]);
+     context.lineTo(frame.clickX[i], frame.clickY[i]);
      context.closePath();
      context.stroke();
   }
 };
 
+// var drawPrevFrame = function () {
+//   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
-var clearFrame = function() {
+//   context.strokeStyle = '#FF0000';
+//   context.lineJoin = "round";
+//   context.lineWidth = 7;
+      
+//   for(var i=0; i < clickX.length; i++) {    
+//     context.beginPath();
+//     if(clickDrag[i] && i){
+//       context.moveTo(clickX[i-1], clickY[i-1]);
+//      }else{
+//        context.moveTo(clickX[i]-1, clickY[i]);
+//      }
+//      context.lineTo(clickX[i], clickY[i]);
+//      context.closePath();
+//      context.stroke();
+//   }
+// };
+
+
+var clearCurrentFrame = function() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-  clickX = [];
-  clickY = [];
-  clickDrag = [];
+  currentFrame.clickX = [];
+  currentFrame.clickY = [];
+  currentFrame.clickDrag = [];
 };
 
 var play = function() {
-  console.log(speed);
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
   var image = new Image();
   image.onload = function() {
     context.drawImage(image, 0, 0);
   };
-  image.src = localStorage.getItem('frame' + currentFrame);
-  currentFrame++;
-  if (currentFrame > framesSaved) currentFrame = 1;
+  image.src = localStorage.getItem('frame' + currentFrameID);
+  currentFrameID++;
+  if (currentFrameID > framesSaved) currentFrameID = 1;
+};
+
+var setPreviousFrame = function() {
+  var prevClickX = currentFrame.clickX.slice();
+  var prevClickY = currentFrame.clickY.slice();
+  var prevClickDrag = currentFrame.clickDrag.slice();
 };
 
 $('#frameView').mousedown(function(e){
@@ -60,13 +99,13 @@ $('#frameView').mousedown(function(e){
 
   paint = true;
   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-  redraw();
+  redraw(currentFrame);
 });
 
 $('#frameView').mousemove(function(e){
   if(paint){
     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-    redraw();
+    redraw(currentFrame);
   }
 });
 
@@ -85,9 +124,9 @@ $('#saveFrame').on('click', function() {
   localStorage.setItem("frame" + framesSaved, image.src);
   $('#view-saved-frames').append('<img src=' + image.src + '>');
   $('#count-saved-frames').text(framesSaved);
-  clearFrame();
+  setPreviousFrame();
+  clearCurrentFrame();
 });
-
 
 $('#playButton').on('click', function() {
   if (framesSaved <= 0) {
@@ -100,8 +139,8 @@ $('#playButton').on('click', function() {
 
 $('#stopButton').on('click', function() {
   clearInterval(playInterval);
-  clearFrame();
-  currentFrame = 1;
+  clearCurrentFrame();
+  currentFrameID = 1;
   framesSaved = 0;
   $('#count-saved-frames').text(framesSaved);
   $('#view-saved-frames').empty();
